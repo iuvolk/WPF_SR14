b3225634255da301eeaec98ab8b6db6413ad249d
Юлия Волкова <volktul@gmail.com>
2023-01-07 00:43:38 +0300

    Добавьте файлы проекта.

---
+++ WPF_SR14/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_SR14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProdName = "Бананы",
+                ProdPrice = 70,
+                ProdImage = "Data/Банан.png",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Кокосы",
+                ProdPrice = 120,
+                ProdImage = "Data/Кокос.png",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Ананасы",
+                ProdPrice = 260,
+                ProdImage = "Data/Ананас.png",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Книги",
+                ProdPrice = 320,
+                ProdImage = "Data/Книга.png",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Рамки",
+                ProdPrice = 150,
+                ProdImage = "Data/Рамка.png",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Растения",
+                ProdPrice = 500,
+                ProdImage = "Data/Растение.png",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            listBox.ItemsSource = products;
+        }
+    }
+}
